e12ac4a88c3659c1d07b4015146f721d5b4b01f6
Gyorgy Borz <borzgy95@gmail.com>
2021-11-27 12:15:07 +0100

    switch between front facing and rear camera

--- HandAndBody/CameraViewController.swift
+++ HandAndBody/CameraViewController.swift
@@ -13,8 +13,14 @@ class CameraViewController: UIViewController {
     private var cameraView: CameraView { view as! CameraView }
 
     private let videoDataOutputQueue = DispatchQueue(label: "CameraFeedDataOutput", qos: .userInteractive)
-    private var cameraFeedSession: AVCaptureSession?
+    private var cameraFeedSession = AVCaptureSession()
+    private let videoDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera, .builtInDualCamera, .builtInTrueDepthCamera],
+                                                                               mediaType: .video, position: .unspecified)
+    @objc dynamic var videoDeviceInput: AVCaptureDeviceInput!
+    @objc dynamic var videoDeviceOutput: AVCaptureVideoDataOutput!
+
     private var handPoseRequest = VNDetectHumanHandPoseRequest()
+    private var bodyPoseRequest = VNDetectHumanBodyPoseRequest()
 
     private let bottomStack = UIStackView()
     private let handButton = UIButton()
@@ -41,11 +47,7 @@ class CameraViewController: UIViewController {
 
     private var isFrontFacingCamera: Bool = true {
         didSet {
-            if isFrontFacingCamera {
-                print("todo")
-            } else {
-                print("todo")
-            }
+            changeCamera()
         }
     }
 
@@ -60,19 +62,17 @@ class CameraViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         do {
-            if cameraFeedSession == nil {
-                cameraView.previewLayer.videoGravity = .resizeAspectFill
-                try setupAVSession()
-                cameraView.previewLayer.session = cameraFeedSession
-            }
-            cameraFeedSession?.startRunning()
+            cameraView.previewLayer.videoGravity = .resizeAspectFill
+            try setupAVSession()
+            cameraView.previewLayer.session = cameraFeedSession
+            cameraFeedSession.startRunning()
         } catch {
             AppError.display(error, inViewController: self)
         }
     }
 
     override func viewWillDisappear(_ animated: Bool) {
-        cameraFeedSession?.stopRunning()
+        cameraFeedSession.stopRunning()
         super.viewWillDisappear(animated)
     }
 
@@ -115,37 +115,137 @@ class CameraViewController: UIViewController {
     }
 
     func setupAVSession() throws {
-        // Select a front facing camera, make an input.
-        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
-            throw AppError.captureSessionSetup(reason: "Could not find a front facing camera.")
+        cameraFeedSession.beginConfiguration()
+
+        // Add video input.
+        do {
+            var defaultVideoDevice: AVCaptureDevice?
+
+            // Choose the back dual camera, if available, otherwise default to a wide angle camera.
+
+            if let dualCameraDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+                defaultVideoDevice = dualCameraDevice
+            } else if let backCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+                // If a rear dual camera is not available, default to the rear wide angle camera.
+                defaultVideoDevice = backCameraDevice
+            } else if let frontCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
+                // If the rear wide angle camera isn't available, default to the front wide angle camera.
+                defaultVideoDevice = frontCameraDevice
+            }
+            guard let videoDevice = defaultVideoDevice else {
+                print("Default video device is unavailable.")
+                cameraFeedSession.commitConfiguration()
+                return
+            }
+            let videoDeviceInput = try AVCaptureDeviceInput(device: videoDevice)
+
+            if cameraFeedSession.canAddInput(videoDeviceInput) {
+                cameraFeedSession.addInput(videoDeviceInput)
+                self.videoDeviceInput = videoDeviceInput
+
+                DispatchQueue.main.async {
+                    let initialVideoOrientation: AVCaptureVideoOrientation = .portrait
+                    self.cameraView.previewLayer.connection?.videoOrientation = initialVideoOrientation
+                }
+            } else {
+                print("Couldn't add video device input to the session.")
+                cameraFeedSession.commitConfiguration()
+                return
+            }
+
+            let videoDeviceOutput = AVCaptureVideoDataOutput()
+            if cameraFeedSession.canAddOutput(videoDeviceOutput) {
+                cameraFeedSession.addOutput(videoDeviceOutput)
+                // Add a video data output.
+                videoDeviceOutput.alwaysDiscardsLateVideoFrames = true
+                videoDeviceOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+                videoDeviceOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+            } else {
+                cameraFeedSession.commitConfiguration()
+                throw AppError.captureSessionSetup(reason: "Could not add video data output to the session")
+            }
+        } catch {
+            print("Couldn't create video device input: \(error)")
+            cameraFeedSession.commitConfiguration()
+            return
         }
 
-        guard let deviceInput = try? AVCaptureDeviceInput(device: videoDevice) else {
-            throw AppError.captureSessionSetup(reason: "Could not create video device input.")
-        }
+        cameraFeedSession.commitConfiguration()
+    }
 
-        let session = AVCaptureSession()
-        session.beginConfiguration()
-        session.sessionPreset = AVCaptureSession.Preset.high
+    func changeCamera() {
+        switchCameraButton.isEnabled = false
+        videoDataOutputQueue.async {
+            let currentVideoDevice = self.videoDeviceInput.device
+            let currentPosition = currentVideoDevice.position
 
-        // Add a video input.
-        guard session.canAddInput(deviceInput) else {
-            throw AppError.captureSessionSetup(reason: "Could not add video device input to the session")
-        }
-        session.addInput(deviceInput)
+            let preferredPosition: AVCaptureDevice.Position
+            let preferredDeviceType: AVCaptureDevice.DeviceType
 
-        let dataOutput = AVCaptureVideoDataOutput()
-        if session.canAddOutput(dataOutput) {
-            session.addOutput(dataOutput)
-            // Add a video data output.
-            dataOutput.alwaysDiscardsLateVideoFrames = true
-            dataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
-            dataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
-        } else {
-            throw AppError.captureSessionSetup(reason: "Could not add video data output to the session")
+            switch currentPosition {
+            case .unspecified, .front:
+                preferredPosition = .back
+                preferredDeviceType = .builtInDualCamera
+
+            case .back:
+                preferredPosition = .front
+                preferredDeviceType = .builtInTrueDepthCamera
+
+            @unknown default:
+                print("Unknown capture position. Defaulting to back, dual-camera.")
+                preferredPosition = .back
+                preferredDeviceType = .builtInDualCamera
+            }
+            let devices = self.videoDeviceDiscoverySession.devices
+            var newVideoDevice: AVCaptureDevice?
+
+            // First, seek a device with both the preferred position and device type. Otherwise, seek a device with only the preferred position.
+            if let device = devices.first(where: { $0.position == preferredPosition && $0.deviceType == preferredDeviceType }) {
+                newVideoDevice = device
+            } else if let device = devices.first(where: { $0.position == preferredPosition }) {
+                newVideoDevice = device
+            }
+
+            if let videoDevice = newVideoDevice {
+                do {
+                    let videoDeviceInput = try AVCaptureDeviceInput(device: videoDevice)
+
+                    self.cameraFeedSession.beginConfiguration()
+
+                    // Remove the existing device input first, because AVCaptureSession doesn't support
+                    // simultaneous use of the rear and front cameras.
+                    self.cameraFeedSession.removeInput(self.videoDeviceInput)
+
+                    if self.cameraFeedSession.canAddInput(videoDeviceInput) {
+                        self.cameraFeedSession.addInput(videoDeviceInput)
+                        self.videoDeviceInput = videoDeviceInput
+                    } else {
+                        self.cameraFeedSession.addInput(self.videoDeviceInput)
+                    }
+
+                    self.cameraFeedSession.outputs.forEach { self.cameraFeedSession.removeOutput($0) }
+                    self.videoDeviceOutput = AVCaptureVideoDataOutput()
+                    if self.cameraFeedSession.canAddOutput(self.videoDeviceOutput) {
+                        self.cameraFeedSession.addOutput(self.videoDeviceOutput)
+                        // Add a video data output.
+                        self.videoDeviceOutput.alwaysDiscardsLateVideoFrames = true
+                        self.videoDeviceOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+                        self.videoDeviceOutput.setSampleBufferDelegate(self, queue: self.videoDataOutputQueue)
+                    } else {
+                        self.cameraFeedSession.commitConfiguration()
+                        throw AppError.captureSessionSetup(reason: "Could not add video data output to the session")
+                    }
+
+                    self.cameraFeedSession.commitConfiguration()
+                } catch {
+                    print("Error occurred while creating video device input: \(error)")
+                }
+            }
+
+            DispatchQueue.main.async {
+                self.switchCameraButton.isEnabled = true
+            }
         }
-        session.commitConfiguration()
-        cameraFeedSession = session
     }
 
     func processPoints(hands: [Hand]) {
@@ -290,7 +390,7 @@ extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
                 hands.append(hand)
             }
         } catch {
-            cameraFeedSession?.stopRunning()
+            cameraFeedSession.stopRunning()
             let error = AppError.visionError(error: error)
             DispatchQueue.main.async {
                 error.displayInViewController(self)
